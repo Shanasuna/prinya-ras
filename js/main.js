e7daf9e4f76c772fdfaca8a231254fd0acbc9905
Warakorn Chanprasopchai <warakorn9z@gmail.com>
2013-09-18 09:50:21 +0700

    Last Code

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(function() {
+	$("input#keyword").bind("input paste", function() {
+		console.log( $("input#keyword").val());
+		$.ajax( {
+			url : "/AjaxSearch",
+			type: "POST",
+			data: {
+				"keyword" : $("input#keyword").val(),
+			},
+			context : this,
+			cache: false,
+			success: function(result) {
+				console.log(result);
+				$("#tableAjaxContent").html(result);
+			}
+		});
+	});
+});
